40229800d60b761f5dbb1f40561a5a3d5f720014
Aayush GC Bhujel <aayushgcbhujel@gmail.com>
2018-07-24 15:24:59 +0545

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  animations: [
+
+    trigger('goals',[
+      transition('* => *', [
+        query(':enter', style({opacity: 0}), {optional: true}),
+
+        query(':enter', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: 0.5, transform: 'translateY(35px)', offset: .3}),
+            style({opacity: 1, transform: 'translateY(0)', offset: 1})
+          ]))
+        ]), {optional: true}),
+
+        query(':leave', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+            style({opacity: 0.5, transform: 'translateY(35px)', offset: .3}),
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 1})
+          ]))
+        ]), {optional: true})
+      ])
+    ])
+
+  ]
+})
+export class HomeComponent implements OnInit {
+
+  itemCount: number;
+  btnText: string = "Add an Item";
+  goalText: string = '';
+  goals = [];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.itemCount = this.goals.length;
+  }
+
+  addItem(){
+    if (this.goalText.length != 0){
+      this.goals.push(this.goalText);
+      this.goalText = '';
+      this.itemCount = this.goals.length;
+    }
+    
+  }
+
+  removeItem(i){
+    this.goals.splice(i,1);
+    this.itemCount = this.goals.length;
+  }
+
+}
